c05986cf9c39da941f2b74223e439efb03744456
fun <jun@mankei.cc>
2024-05-03 16:40:14 +0800

    -

--- cron.js
+++ cron.js
@@ -2,6 +2,7 @@ const cron = require("node-cron");
 
 cron.schedule("*/5 * * * * *", function () {
   try {
+    console.log(new Date());
     fetch("https://uka.minapp.xin/api/job").then();
   } catch (err) {}
 });
