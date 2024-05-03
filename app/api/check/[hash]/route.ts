fec0f3a12259f2171ef7cf1450adef2984098ee5
fun <jun@mankei.cc>
2024-05-03 23:36:39 +0800

    -

--- app/api/check/[hash]/route.ts
+++ app/api/check/[hash]/route.ts
@@ -1,1 +1,14 @@
-export async function GET(request) {}
+import prisma from "@/lib/prisma";
+
+export async function GET(request, ctx) {
+  let { hash } = ctx.params;
+  let order = await prisma.order.findFirst({ where: { hash } });
+  if (!order?.payed) {
+    return Response.json({ status: true, result: "nopay" });
+  }
+  if (order?.used) {
+    return Response.json({ status: true, result: "used" });
+  }
+
+  return Response.json({ status: true, result: "success" });
+}
