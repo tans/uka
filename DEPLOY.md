4ff6a77996ff2b551aef964f1a5d78366f69e751
fun <jun@mankei.cc>
2024-05-09 14:51:34 +0800

    add deploy md

---
+++ DEPLOY.md
@@ -0,0 +1,8 @@
+# UKA独立部署说明文档
+
+1. 参考.env.example 文件配置自己的 .env
+2. example上是使用BSC测试链的USDT，自行填写正确的收款代币BEP20合约地址
+3. 自行申请BSCSCAN_KEY
+4. 配置数据库
+
+开发和运行参考标准的next.js 项目
--- cron.js
+++ cron.js
@@ -3,6 +3,6 @@ const cron = require("node-cron");
 cron.schedule("*/5 * * * * *", function () {
   try {
     console.log(new Date());
-    fetch("https://uka.minapp.xin/api/job").then();
+    fetch(process.env.HOST + "/api/job").then();
   } catch (err) {}
 });
